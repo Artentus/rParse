122978d37400f8c6c68cc040471add9c14d70012
Mathis Rech <mathisrech@gmx.de>
2021-12-10 16:26:05 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,264 @@
+#![feature(pattern)]
+
+use std::rc::Rc;
+pub mod text;
+
+#[derive(Debug)]
+#[must_use]
+pub enum ParseResult<T, I: Clone> {
+    /// The input matched the parser pattern.
+    Match(I, T),
+    /// The input did not match the parser pattern.
+    NoMatch,
+    /// The input matched the parser pattern but was malformed or invalid.
+    Err(I),
+}
+
+/// Parses some input into structured data.
+#[must_use]
+pub struct Parser<T, I: Clone> {
+    parse_fn: Rc<dyn Fn(I) -> ParseResult<T, I>>,
+}
+impl<T, I: Clone> Parser<T, I> {
+    /// Creates a new parser from a function.
+    pub fn new<F: 'static + Fn(I) -> ParseResult<T, I>>(parse_fn: F) -> Self {
+        Self {
+            parse_fn: Rc::new(parse_fn),
+        }
+    }
+
+    /// Runs the parser on the given input.
+    pub fn run(&self, input: I) -> ParseResult<T, I> {
+        (self.parse_fn)(input)
+    }
+}
+impl<T, I: Clone> Clone for Parser<T, I> {
+    fn clone(&self) -> Self {
+        Self {
+            parse_fn: Rc::clone(&self.parse_fn),
+        }
+    }
+}
+
+/// Creates a parser that always matches and returns the specified value.
+pub fn always<T: 'static + Clone, I: Clone>(value: T) -> Parser<T, I> {
+    Parser::new(move |input: I| ParseResult::Match(input, value.clone()))
+}
+
+/// Creates a parser that never matches.
+pub fn never<T, I: Clone>() -> Parser<T, I> {
+    Parser::new(move |_input: I| ParseResult::NoMatch)
+}
+
+/// Creates a parser that errors if the inner parser did not match.
+pub fn require<T: 'static, I: 'static + Clone>(parser: Parser<T, I>) -> Parser<T, I> {
+    Parser::new(move |input: I| match parser.run(input.clone()) {
+        ParseResult::Match(remaining, result) => ParseResult::Match(remaining, result),
+        ParseResult::NoMatch => ParseResult::Err(input),
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
+
+/// Creates a parser that doesn't match if the given validation function returns false.
+pub fn verify<T: 'static, I: 'static + Clone>(
+    parser: Parser<T, I>,
+    f: impl Fn(&T) -> bool + 'static,
+) -> Parser<T, I> {
+    Parser::new(move |input: I| match parser.run(input.clone()) {
+        ParseResult::Match(remaining, result) => {
+            if f(&result) {
+                ParseResult::Match(remaining, result)
+            } else {
+                ParseResult::NoMatch
+            }
+        }
+        ParseResult::NoMatch => ParseResult::NoMatch,
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
+
+/// Creates a parser that returns None if the inner parser did not match.
+pub fn opt<T: 'static, I: 'static + Clone>(parser: Parser<T, I>) -> Parser<Option<T>, I> {
+    Parser::new(move |input: I| match parser.run(input.clone()) {
+        ParseResult::Match(remaining, result) => ParseResult::Match(remaining, Some(result)),
+        ParseResult::NoMatch => ParseResult::Match(input, None),
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
+
+/// Creates a parser that runs the given inner parser and returns a specific value if it matches.
+pub fn map_to<T1: 'static, T2: 'static + Clone, I: 'static + Clone>(
+    parser: Parser<T1, I>,
+    value: T2,
+) -> Parser<T2, I> {
+    Parser::new(move |input: I| match parser.run(input) {
+        ParseResult::Match(remaining, _) => ParseResult::Match(remaining, value.clone()),
+        ParseResult::NoMatch => ParseResult::NoMatch,
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
+
+/// Creates a parser that runs the given inner parser and maps its result using a mapping function if it matches.
+pub fn map<T1: 'static, T2, I: 'static + Clone>(
+    parser: Parser<T1, I>,
+    mapping: impl Fn(T1) -> T2 + 'static,
+) -> Parser<T2, I> {
+    Parser::new(move |input: I| match parser.run(input) {
+        ParseResult::Match(remaining, result) => ParseResult::Match(remaining, mapping(result)),
+        ParseResult::NoMatch => ParseResult::NoMatch,
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
+
+/// Creates a parser from a given inner parser that matches a list and folds its result into a single value using an aggregator function.
+pub fn fold<
+    T1: 'static,
+    Iter: 'static + IntoIterator<Item = &'static T1>,
+    T2: 'static + Clone,
+    I: 'static + Clone,
+>(
+    parser: Parser<Iter, I>,
+    init: T2,
+    f: impl Fn(T2, &T1) -> T2 + 'static,
+) -> Parser<T2, I> {
+    Parser::new(move |input: I| match parser.run(input) {
+        ParseResult::Match(remaining, result) => ParseResult::Match(
+            remaining,
+            result
+                .into_iter()
+                .fold(init.clone(), |acc, item| f(acc, item)),
+        ),
+        ParseResult::NoMatch => ParseResult::NoMatch,
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
+
+/// Creates a parser that matches any of the given parsers. Parsers are matched in order.
+pub fn choice<T: 'static, I: 'static + Clone, const N: usize>(
+    parsers: [Parser<T, I>; N],
+) -> Parser<T, I> {
+    Parser::new(move |input: I| {
+        for parser in parsers.clone() {
+            match parser.run(input.clone()) {
+                ParseResult::Match(remaining, result) => {
+                    return ParseResult::Match(remaining, result)
+                }
+                ParseResult::NoMatch => continue,
+                ParseResult::Err(location) => return ParseResult::Err(location),
+            }
+        }
+
+        ParseResult::NoMatch
+    })
+}
+
+/// Creates a parser that matches all of the given parsers, in order.
+pub fn sequence<T: 'static, I: 'static + Clone, const N: usize>(
+    parsers: [Parser<T, I>; N],
+) -> Parser<Vec<T>, I> {
+    Parser::new(move |input: I| {
+        let mut results = Vec::with_capacity(N);
+        let mut remaining = input;
+
+        for parser in parsers.clone() {
+            match parser.run(remaining) {
+                ParseResult::Match(new_remaining, result) => {
+                    remaining = new_remaining;
+                    results.push(result);
+                }
+                ParseResult::NoMatch => return ParseResult::NoMatch,
+                ParseResult::Err(location) => return ParseResult::Err(location),
+            }
+        }
+
+        ParseResult::Match(remaining, results)
+    })
+}
+
+/// Creates a parser that matches two parsers in sequence.
+pub fn and_then<T1: 'static, T2: 'static, I: 'static + Clone>(
+    parser1: Parser<T1, I>,
+    parser2: Parser<T2, I>,
+) -> Parser<(T1, T2), I> {
+    Parser::new(move |input: I| match parser1.run(input) {
+        ParseResult::Match(remaining, result1) => match parser2.run(remaining) {
+            ParseResult::Match(remaining, result2) => {
+                ParseResult::Match(remaining, (result1, result2))
+            }
+            ParseResult::NoMatch => ParseResult::NoMatch,
+            ParseResult::Err(location) => ParseResult::Err(location),
+        },
+        ParseResult::NoMatch => ParseResult::NoMatch,
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
+
+/// Creates a parser that matches the given inner parser N exactly times.
+pub fn repeat<T: 'static, I: 'static + Clone>(
+    parser: Parser<T, I>,
+    count: usize,
+) -> Parser<Vec<T>, I> {
+    Parser::new(move |input: I| {
+        let mut results = Vec::with_capacity(count);
+        let mut remaining = input;
+
+        for _ in 0..count {
+            match parser.run(remaining) {
+                ParseResult::Match(new_remaining, result) => {
+                    remaining = new_remaining;
+                    results.push(result);
+                }
+                ParseResult::NoMatch => return ParseResult::NoMatch,
+                ParseResult::Err(location) => return ParseResult::Err(location),
+            }
+        }
+
+        ParseResult::Match(remaining, results)
+    })
+}
+
+/// Creates a parser that matches the given inner parser zero or more times.
+pub fn many<T: 'static, I: 'static + Clone>(parser: Parser<T, I>) -> Parser<Vec<T>, I> {
+    Parser::new(move |input: I| {
+        let mut results = Vec::new();
+        let mut remaining = input;
+
+        loop {
+            match parser.run(remaining.clone()) {
+                ParseResult::Match(new_remaining, result) => {
+                    remaining = new_remaining;
+                    results.push(result);
+                }
+                ParseResult::NoMatch => break,
+                ParseResult::Err(location) => return ParseResult::Err(location),
+            }
+        }
+
+        ParseResult::Match(remaining, results)
+    })
+}
+
+/// Creates a parser that matches the given inner parser one or more times.
+pub fn many1<T: 'static, I: 'static + Clone>(parser: Parser<T, I>) -> Parser<Vec<T>, I> {
+    Parser::new(move |input: I| match parser.run(input) {
+        ParseResult::Match(mut remaining, result) => {
+            let mut results = Vec::new();
+            results.push(result);
+
+            loop {
+                match parser.run(remaining.clone()) {
+                    ParseResult::Match(new_remaining, result) => {
+                        remaining = new_remaining;
+                        results.push(result);
+                    }
+                    ParseResult::NoMatch => break,
+                    ParseResult::Err(location) => return ParseResult::Err(location),
+                }
+            }
+
+            ParseResult::Match(remaining, results)
+        }
+        ParseResult::NoMatch => ParseResult::NoMatch,
+        ParseResult::Err(location) => ParseResult::Err(location),
+    })
+}
---
+++ src/text.rs
@@ -0,0 +1,315 @@
+use super::*;
+use std::ops::{Bound, RangeBounds};
+
+trait StringUtils {
+    #[must_use]
+    fn substring(&self, range: impl RangeBounds<usize>) -> &Self;
+}
+impl StringUtils for str {
+    fn substring(&self, range: impl RangeBounds<usize>) -> &Self {
+        let char_start_inclusive = match range.start_bound() {
+            Bound::Included(start) => *start,
+            Bound::Excluded(start) => start.saturating_sub(1),
+            Bound::Unbounded => 0,
+        };
+
+        let char_end_exclusive = match range.end_bound() {
+            Bound::Included(end) => Some(end.saturating_add(1)),
+            Bound::Excluded(end) => Some(*end),
+            Bound::Unbounded => None,
+        };
+
+        let char_len = match char_end_exclusive {
+            Some(char_end_exclusive) => {
+                Some(char_end_exclusive.saturating_sub(char_start_inclusive))
+            }
+            None => None,
+        };
+
+        let mut chars = self.char_indices();
+        let byte_start_inclusive = chars.nth(char_start_inclusive).unwrap().0;
+        let byte_end_exclusive: usize = match char_len {
+            Some(char_len) => {
+                if char_len == 0 {
+                    byte_start_inclusive
+                } else {
+                    chars.nth(char_len - 1).unwrap().0
+                }
+            }
+            None => self.len(),
+        };
+
+        &self[byte_start_inclusive..byte_end_exclusive]
+    }
+}
+
+pub trait CharCollection {
+    #[must_use]
+    fn contains(&self, c: char) -> bool;
+}
+impl CharCollection for &[char] {
+    fn contains(&self, c: char) -> bool {
+        (*self).contains(&c)
+    }
+}
+impl CharCollection for Vec<char> {
+    fn contains(&self, c: char) -> bool {
+        self.as_slice().contains(&c)
+    }
+}
+impl CharCollection for &str {
+    fn contains(&self, c: char) -> bool {
+        self.chars().any(|other| c == other)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct TextPosition {
+    pub abs: usize,
+    pub line: usize,
+    pub column: usize,
+}
+
+#[derive(Clone, Copy)]
+pub struct TextInput {
+    text: &'static str,
+    pos: TextPosition,
+}
+impl TextInput {
+    #[must_use]
+    pub const fn new(text: &'static str) -> Self {
+        Self {
+            text,
+            pos: TextPosition {
+                abs: 0,
+                line: 0,
+                column: 0,
+            },
+        }
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn as_str(&self) -> &str {
+        self.text
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn len(&self) -> usize {
+        self.text.len()
+    }
+
+    #[inline]
+    #[must_use]
+    pub const fn pos(&self) -> &TextPosition {
+        &self.pos
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn starts_with<'a, P: std::str::pattern::Pattern<'a>>(&'a self, pat: P) -> bool {
+        self.text.starts_with(pat)
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn chars(&self) -> std::str::Chars {
+        self.text.chars()
+    }
+
+    #[must_use]
+    pub fn advance(&self, count: usize) -> Self {
+        if count == 0 {
+            self.clone()
+        } else {
+            let advance_str = self.text.substring(..count);
+            let mut line = self.pos.line;
+            let mut column = self.pos.column;
+            for c in advance_str.chars() {
+                if c == '\n' {
+                    line += 1;
+                    column = 0;
+                } else {
+                    column += 1;
+                }
+            }
+
+            Self {
+                text: self.text.substring(count..),
+                pos: TextPosition {
+                    abs: self.pos.abs + count,
+                    line,
+                    column,
+                },
+            }
+        }
+    }
+}
+
+pub type TextParser<T> = Parser<T, TextInput>;
+
+/// Creates a parser that matches any char.
+pub fn any() -> TextParser<char> {
+    Parser::new(|input: TextInput| match input.chars().nth(0) {
+        Some(c) => ParseResult::Match(input.advance(1), c),
+        None => ParseResult::NoMatch,
+    })
+}
+
+/// Creates a parser that matches a specific char.
+pub fn char(c: char) -> TextParser<char> {
+    Parser::new(move |input: TextInput| {
+        if input.starts_with(c) {
+            ParseResult::Match(input.advance(1), c)
+        } else {
+            ParseResult::NoMatch
+        }
+    })
+}
+
+/// Creates a parser that matches a char based on a predicate function.
+pub fn matches(predicate: impl Fn(&char) -> bool + 'static) -> TextParser<char> {
+    Parser::new(move |input: TextInput| match input.chars().nth(0) {
+        Some(c) => {
+            if predicate(&c) {
+                ParseResult::Match(input.advance(1), c)
+            } else {
+                ParseResult::NoMatch
+            }
+        }
+        None => ParseResult::NoMatch,
+    })
+}
+
+/// Creates a parser that matches one of the given chars. Chars are matched in order.
+pub fn one_of<L: 'static + CharCollection>(list: L) -> TextParser<char> {
+    Parser::new(move |input: TextInput| match input.chars().nth(0) {
+        Some(c) => {
+            if list.contains(c) {
+                ParseResult::Match(input.advance(1), c)
+            } else {
+                ParseResult::NoMatch
+            }
+        }
+        None => ParseResult::NoMatch,
+    })
+}
+
+/// Creates a parser that matches a specific string.
+pub fn string(s: &'static str) -> TextParser<String> {
+    Parser::new(move |input: TextInput| {
+        if input.starts_with(s) {
+            ParseResult::Match(input.advance(s.len()), s.to_string())
+        } else {
+            ParseResult::NoMatch
+        }
+    })
+}
+
+/// Creates a parser that matches the end of the input.
+pub fn eof() -> TextParser<()> {
+    Parser::new(move |input: TextInput| {
+        if input.len() == 0 {
+            ParseResult::Match(input, ())
+        } else {
+            ParseResult::NoMatch
+        }
+    })
+}
+
+pub fn collect_string(parser: TextParser<Vec<char>>) -> TextParser<String> {
+    map(parser, |chars| chars.iter().collect())
+}
+
+pub fn whitespace() -> TextParser<char> {
+    matches(char::is_ascii_whitespace)
+}
+
+pub fn alphabetic() -> TextParser<char> {
+    matches(char::is_ascii_alphabetic)
+}
+
+pub fn alphanumeric() -> TextParser<char> {
+    matches(char::is_ascii_alphanumeric)
+}
+
+pub fn digit() -> TextParser<char> {
+    matches(char::is_ascii_digit)
+}
+
+pub fn hexdigit() -> TextParser<char> {
+    matches(char::is_ascii_hexdigit)
+}
+
+pub fn octdigit() -> TextParser<char> {
+    one_of("01234567")
+}
+
+pub fn bindigit() -> TextParser<char> {
+    one_of("01")
+}
+
+pub fn sign() -> TextParser<char> {
+    one_of("+-")
+}
+
+pub fn newline() -> TextParser<String> {
+    choice([string("\n"), string("\r\n")])
+}
+
+pub fn whitespace0() -> TextParser<String> {
+    collect_string(many(whitespace()))
+}
+
+pub fn whitespace1() -> TextParser<String> {
+    collect_string(many1(whitespace()))
+}
+
+pub fn alphabetic0() -> TextParser<String> {
+    collect_string(many(alphabetic()))
+}
+
+pub fn alphabetic1() -> TextParser<String> {
+    collect_string(many1(alphabetic()))
+}
+
+pub fn alphanumeric0() -> TextParser<String> {
+    collect_string(many(alphanumeric()))
+}
+
+pub fn alphanumeric1() -> TextParser<String> {
+    collect_string(many1(alphanumeric()))
+}
+
+pub fn digit0() -> TextParser<String> {
+    collect_string(many(digit()))
+}
+
+pub fn digit1() -> TextParser<String> {
+    collect_string(many1(digit()))
+}
+
+pub fn hexdigit0() -> TextParser<String> {
+    collect_string(many(hexdigit()))
+}
+
+pub fn hexdigit1() -> TextParser<String> {
+    collect_string(many1(hexdigit()))
+}
+
+pub fn octdigit0() -> TextParser<String> {
+    collect_string(many(octdigit()))
+}
+
+pub fn octdigit1() -> TextParser<String> {
+    collect_string(many1(octdigit()))
+}
+
+pub fn bindigit0() -> TextParser<String> {
+    collect_string(many(bindigit()))
+}
+
+pub fn bindigit1() -> TextParser<String> {
+    collect_string(many1(bindigit()))
+}
